285dbf7a10f18563289aa7f3b67feee0cb0848ef
dsokac <dsokac@foi.hr>
2020-11-11 19:28:19 +0100

    initial commit

---
+++ src/main/java/hr/danisoka/webshopingmrk/WebshopIngmrkApplication.java
@@ -0,0 +1,13 @@
+package hr.danisoka.webshopingmrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopIngmrkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopIngmrkApplication.class, args);
+	}
+
+}
