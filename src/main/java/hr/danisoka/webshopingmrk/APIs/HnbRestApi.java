24ad695e65e579d4d38385e61b29b80f77806e15
dsokac <dsokac@foi.hr>
2020-11-18 22:21:35 +0100

    complete hnb rest api

--- src/main/java/hr/danisoka/webshopingmrk/APIs/HnbRestApi.java
+++ src/main/java/hr/danisoka/webshopingmrk/APIs/HnbRestApi.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -17,7 +16,7 @@ public class HnbRestApi {
 
 	@Autowired
 	private RestTemplate restTemplate;
-	private static final String BASE_URL = "http://api.hnb.hr/tecajn/v1";
+	public static final String BASE_URL = "http://api.hnb.hr/tecajn/v1";
 	public static final String CURRENCY_EUR = "EUR";
 	
 	public HnbRestApi() {
